c1c6669f926c668ec3701dfc4a3be6e4f64472f8
Anton Wahrstätter <51536394+Nerolation@users.noreply.github.com>
2020-12-30 10:34:03 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -315,3 +315,6 @@ USERS['randy'] = USER("randy", IMP.address).__dict__
 
 if __name__ == '__main__':
    app.run(debug=True)
+   gunicorn_logger = logging.getLogger('gunicorn.error')
+   app.logger.handlers = gunicorn_logger.handlers
+   app.logger.setLevel(gunicorn_logger.level)
